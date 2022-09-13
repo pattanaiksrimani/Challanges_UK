6095ac77174d47f734854394e8b63e911590a4f6
pattanaiksrimani <42177354+pattanaiksrimani@users.noreply.github.com>
2022-09-13 15:08:34 +0530

    Update install-apache.sh

--- Challange-1/install-apache.sh
+++ Challange-1/install-apache.sh
@@ -5,8 +5,3 @@ yum install -y httpd.x86_64
 systemctl start httpd.service
 systemctl enable httpd.service
 echo "Hello World from $(hostname -f) !! I am Srimani Pattanaik" > /var/www/html/index.html
-
-
-
-
-https://aws.plainenglish.io/terraform-deploying-a-three-tier-architecture-in-aws-4c8ecce40790
